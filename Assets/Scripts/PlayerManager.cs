d89527317f5f3bb4c1be2f7df13ffe00d1c8ba80
Cyb3rtron <dian.ehrke@gmail.com>
2026-04-21 17:19:10 +0200

    player animations
    walking, falling + jumping

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,12 +6,14 @@ public class PlayerManager : MonoBehaviour
     private PlayerInputSystem playerInputSys; //input system reference
 
     public Rigidbody2D rb;
+    public Animator anim;
     public float moveSpeed = 1f;
     [Header("Jumping")]
     public float jumpforce = 1f;
     public float lowjumpMultiplier = 2f;
     public float fallMultiplier = 3f;
     public bool isGrounded = true;
+    public bool isFalling = false;
 
     private float coyoteTime = 0.1f;
     [SerializeField] private float coyoteTimeCounter = 0.1f;
@@ -35,7 +37,14 @@ public class PlayerManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if(rb==null)
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        if(anim==null)
+        {
+            anim = GetComponent<Animator>();
+        }
     }
 
     // Update is called once per frame
@@ -44,9 +53,9 @@ public class PlayerManager : MonoBehaviour
         Vector2 playerInput = playerInputSys.Player.Move.ReadValue<Vector2>(); //reads the player's input from the input system and turns it into a vector2
         rb.linearVelocity = new Vector2(playerInput.x * moveSpeed, rb.linearVelocity.y);
 
-        if (playerInput != Vector2.zero)
+        if (playerInput.x != 0)
         {
-            //anim.SetBool("isWalking", true);
+            anim.SetBool("isWalking", true);
             if (playerInput.x < 0)
             {
                 transform.rotation = Quaternion.Euler(0f, 180f, 0f); //rotates the player left or right based on input
@@ -59,13 +68,13 @@ public class PlayerManager : MonoBehaviour
             if (!walkingSound.isPlaying)
             {
                 walkingSound.Play();
-            }
+            }*/
 
         }
         else
         {
             anim.SetBool("isWalking", false);
-            walkingSound.Stop();*/
+            //walkingSound.Stop();
         }
 
         Vector3 rayPos = new Vector3(transform.position.x, transform.position.y - rayOffset, transform.position.z);
@@ -79,6 +88,7 @@ public class PlayerManager : MonoBehaviour
         else
         {
             coyoteTimeCounter -= Time.fixedDeltaTime;
+            
         }
 
         if (playerInputSys.Player.Jump.WasPressedThisFrame() && coyoteTimeCounter > 0f)
@@ -90,18 +100,31 @@ public class PlayerManager : MonoBehaviour
         if(rb.linearVelocity.y < 0f) //falling
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+            isFalling = true;
         }
         else if(rb.linearVelocity.y > 0f && !playerInputSys.Player.Jump.IsPressed()) //low jump
         {
             rb.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowjumpMultiplier - 1) * Time.fixedDeltaTime;
         }
+        if(rb.linearVelocity.y > 0f)
+        {
+            isFalling = false;
+        }
+
+        UpdateAnims();
+    }
+
+    void UpdateAnims()
+    {
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isFalling", isFalling);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
-            isGrounded = true;
+            isGrounded = true;            
         }
     }
 }
